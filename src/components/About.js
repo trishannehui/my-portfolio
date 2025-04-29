33380e70048869e139755d79eee53b3555ba6a8c
trishannehui <huidemt@gmail.com>
2025-04-29 03:13:07 -0300

    Add files via upload

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header style={{ padding: "1rem", backgroundColor: "#222", color: "#fff" }}>
+      <h1>My Portfolio</h1>
+      <p>Frontend Developer | React Enthusiast</p>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Projects.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+import React from "react";
+
+function Header() {
+  return (
+    <header style={{ padding: "1rem", backgroundColor: "#222", color: "#fff" }}>
+      <h1>My Portfolio</h1>
+      <p>Frontend Developer | React Enthusiast</p>
+    </header>
+  );
+}
+
+export default Header;
